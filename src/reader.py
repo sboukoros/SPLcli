abed230ad210caf271b0e88581784ce7a78266c0
Spyros <sboukoros@gmail.com>
2022-12-23 05:32:59 +0100

    input cli options and a basic reader Class

---
+++ src/reader.py
@@ -0,0 +1,47 @@
+import csv
+import heapq
+
+
+class Reader:
+
+    def __init__(self, readPath, outputPath,mfip, lfip, eps, _bytes):
+        self.readPath = readPath
+        self.outputPath = outputPath
+        self.mfip = mfip
+        self.lfip = lfip
+        self. eps = eps
+        self._bytes = excbytes
+
+        print(readPath, outputPath, mfip, lfip, eps, excbytes)
+
+    def _returnMfip():
+        pass
+
+
+    def test(self):
+        readPath = self.readPath
+        outputPath = self.outputPath
+        print(readPath)
+        with open(readPath, 'r') as file:
+            with open(outputPath, 'w') as wfile:
+                csvreader = csv.reader(file)
+                csvwriter = csv.writer(wfile)
+                for row in csvreader:
+                    csvwriter.writerow(row)
+
+         #   print(row)
+    
+
+    def main():
+        pass
+
+
+    def heap():
+        pass
+
+
+    def eps():
+        pass
+
+    def sumbytes():
+        pass
